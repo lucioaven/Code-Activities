ab6a14d744a22532aaae88856c27147580f0a52b
Lucio Avendaño <lucio.aven03@gmail.com>
2024-09-24 19:28:49 -0300

    Update Lucio.hs

--- Lucio.hs
+++ Lucio.hs
@@ -12,3 +12,33 @@ sumaLista [] = 0
 sumaLista (x:xs) = x + sumaLista xs
 
 --Ejercicio 9 
+curri :: Int -> Int -> Int
+curri x y = (x^y)
+
+--Ejercicio 10
+currif :: Int -> Int 
+currif x = curri 2 x
+
+--Ejercicio 11
+sumTres :: (Num a) =>  a -> a -> a -> a
+sumTres  x y z = (x+y+z)
+
+--Ejercicio 12
+addFour :: (Num a) => a -> a -> a
+addFour x y = sumTres x y 4
+
+--Ejercicio 15 
+belongs e [] = False
+belongs e ( a : as ) = belongs e as || ( a == e )
+
+--Ejercicio 16
+fib :: Int -> Int 
+fib x
+    |x<=0 = 0 --Caso base
+    |x == 1 = 1 --Caso base 
+    |otherwise = fib(x-1) + fib(x-2) --Hipotesis Inductiva
+
+--Ejercicio 17 
+length' :: [a] -> Int
+length'  [] = 0
+length' ( _ : xs ) = 1 + length' xs
